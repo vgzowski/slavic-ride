955675c6ed9614eeed42251df6b8251f2e8ad89c
bliznetc <bliznetc2006@gmail.com>
2024-05-03 18:53:47 +0200

    created map with react

---
+++ slavic-ride-frontend/slavic-ride/src/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import MapComponent from './components/mapComponent';
+
+function App() {
+  return <MapComponent/>;
+}
+
+export default App;
---
+++ slavic-ride-frontend/slavic-ride/src/components/mapComponent.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {APIProvider, Map} from '@vis.gl/react-google-maps';
+
+function MapComponent () {
+    const apiKey = "AIzaSyCcGid1vTF4zEMmDMWgS5sX3fOxrAtGhDs";
+    const position = {lat: 53.217, lng: 29.183};
+    return (
+        <APIProvider apiKey={apiKey}>
+            <Map
+            style={{width: '100%', height: '700px'}}
+            center={position}
+            zoom={9}
+            />
+        </APIProvider>
+    )
+};
+
+export default MapComponent;
