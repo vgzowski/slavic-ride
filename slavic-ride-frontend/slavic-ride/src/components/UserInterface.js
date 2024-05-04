2c16418ced601fe771da8822cc173ffc12c0df63
Kiryl <melenyakokirill7102005@gmail.com>
2024-05-04 15:42:20 +0200

    Reverted routesComponent.js, have added build route from one to second (input in labels), basic user interface without GUI on page /user

--- slavic-ride-frontend/slavic-ride/src/components/UserInterface.js
+++ slavic-ride-frontend/slavic-ride/src/components/UserInterface.js
@@ -2,69 +2,48 @@ import React, { Component } from 'react';
 import MapComponent from './mapComponent';
 
 class UserInterface extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      source: '',
-      destination: ''
-    };
-  }
-
-  handleSourceChange = (e) => {
-    this.setState({ source: e.target.value });
-  };
-
-  handleDestinationChange = (e) => {
-    this.setState({ destination: e.target.value });
-  };
-
-  handleDirections = () => {
-    const { destination } = this.state;
-    if (destination) {
-      const directionsService = new DirectionsService();
-      directionsService.getDirections(destination);
-    } else {
-      alert('Please enter a destination address');
+    constructor(props) {
+        super(props);
+        this.state = {
+            source: '',
+            destination: ''
+        };
     }
-  };
 
-  render() {
-    const { source, destination } = this.state;
+    handleSourceChange = (e) => {
+        this.setState({ source: e.target.value });
+    }
 
-    return (
-      <div>
-        <MapComponent />
-        <h1>Enter Source and Destination</h1>
-        <div>
-          <label htmlFor="source">Source Address:</label>
-          <input
-            type="text"
-            id="source"
-            value={source}
-            onChange={this.handleSourceChange}
-          />
-        </div>
-        <div>
-          <label htmlFor="destination">Destination Address:</label>
-          <input
-            type="text"
-            id="destination"
-            value={destination}
-            onChange={this.handleDestinationChange}
-          />
-        </div>
-        <button onClick={this.handleDirections}>Get Directions</button>
-      </div>
-    );
-  }
-}
+    handleDestinationChange = (e) => {
+        this.setState({ destination: e.target.value });
+    }
 
-class DirectionsService {
-  getDirections(address) {
-    console.log('Getting directions for:', address);
-    // Implement logic to get directions based on the address
-    // For demonstration, I'm just logging the address to the console
-  }
+    render () {
+        const { source, destination } = this.state;
+
+        return (
+            <div>
+                <MapComponent userLocation={source} userDestination={destination} />
+                <h1>
+                    Enter Source and Destination
+                </h1>
+
+                <div>
+                    <label htmlFor="source">
+                        Source Address:
+                    </label>
+                    <input type="text" id="source" value={source} onChange={this.handleSourceChange} />
+                </div>
+
+                <div>
+                    <label htmlFor="destination">
+                        Destination Address:
+                    </label>
+                    <input type="text" id="destination" value={destination} onChange={this.handleDestinationChange} />
+                </div>         
+            </div>
+        );
+    }
 }
 
 export default UserInterface;
