634b4a41e9c77a2a88df831aff6d59f064fd6447
vgzowski <vovmorozov2@gmail.com>
2024-05-11 14:15:48 +0200

    Update PassengerService

--- slavic-ride-java/src/main/java/Slavic/Ride/MMM/Service/PassengerService.java
+++ slavic-ride-java/src/main/java/Slavic/Ride/MMM/Service/PassengerService.java
@@ -2,6 +2,7 @@ package Slavic.Ride.MMM.Service;
 
 import Slavic.Ride.MMM.Repo.DriverRepo;
 import Slavic.Ride.MMM.Repo.PassengerRepo;
+import Slavic.Ride.MMM.Service.DriverService;
 import Slavic.Ride.MMM.User.Driver;
 import Slavic.Ride.MMM.User.Passenger;
 import jakarta.transaction.Transactional;
@@ -20,6 +21,7 @@ import java.util.*;
 public class PassengerService {
     private final PassengerRepo passengerRepo;
     private final DriverRepo driverRepo;
+    private final DriverService driverService;
 
     public Passenger createPassenger(Passenger passenger) {
         log.info("Creating passenger: {}", passenger);
@@ -41,35 +43,9 @@ public class PassengerService {
 
     public Driver findClosestDriver(String id) {
         log.info("Finding the closest driver to the passenger");
-
         Passenger passenger = passengerRepo.findPassengerById(id)
                 .orElseThrow(() -> new IllegalArgumentException("Passenger not found"));
 
-        List<Driver> drivers = driverRepo.findAllNotTaken();
-        Driver closestDriver = null;
-        double closestDistance = Double.MAX_VALUE;
-
-        for (Driver driver : drivers) {
-            double distance = calculateDistance(passenger.getLocation(), driver.getLocation());
-            if (distance < closestDistance) {
-                closestDriver = driver;
-                closestDistance = distance;
-            }
-        }
-
-        return closestDriver;
-    }
-
-    private static double rEarth = 6371;
-
-    private static double calculateDistance(Location location1, Location location2) {
-        double latDistance = Math.toRadians(location2.getLatitude() - location1.getLatitude());
-        double lonDistance = Math.toRadians(location2.getLongitude() - location1.getLongitude());
-        double a = Math.sin(latDistance / 2) * Math.sin(latDistance / 2)
-                + Math.cos(Math.toRadians(location1.getLatitude())) * Math.cos(Math.toRadians(location2.getLatitude()))
-                * Math.sin(lonDistance / 2) * Math.sin(lonDistance / 2);
-        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-        double distance = rEarth * c;
-        return distance;
+        return driverService.findClosestDriverByLocation(passenger.getLocation());
     }
 }
