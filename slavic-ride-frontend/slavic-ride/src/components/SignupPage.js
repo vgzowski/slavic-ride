5720aa742e37f52f361aa0fa74fd7b749568297d
Kiryl <melenyakokirill7102005@gmail.com>
2024-05-25 18:53:45 +0200

    Complete SignUp

--- slavic-ride-frontend/slavic-ride/src/components/SignupPage.js
+++ slavic-ride-frontend/slavic-ride/src/components/SignupPage.js
@@ -1,6 +1,7 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import bcrypt from 'bcryptjs';
+import { useNavigate } from 'react-router-dom';
 
 const SignupPage = () => {
     const [name, setName] = useState('');
@@ -9,30 +10,46 @@ const SignupPage = () => {
     const [car, setCar] = useState('');
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
+    const [id, setId] = useState('');
+    const [role, setRole] = useState(false);
+    const navigate = useNavigate();
 
     const generateId = async () => {
         const response = await axios.get('http://localhost:8080/auth/generateId');
-        return response.data.id;
+        return response.data;
     }
 
-    const handleSubmit = async (e) => {
-        const id = await generateId();
-        e.preventDefault();
+    useEffect(() => {
+        const fetchId = async () => {
+            if (id === '') {
+                const newId = await generateId();
+                setId(newId);
+                console.log(newId);
+            }
+        };
+        fetchId();
+    }, [id]);
+
+    const handleSubmit = async () => {
         const hashedPassword = await bcrypt.hash(password, 10);
         try {
             const requestBody = {
-                "name": name,
-                "email": email,
-                "phone": phone,
-                "id": id,
-                "car": car,
-                "username": username,
-                "password": hashedPassword,
+                name: name,
+                email: email,
+                phone: phone,
+                id: id,
+                car: role ? car : '', // Only include car if role is true
+                username: username,
+                password: hashedPassword,
             };
             console.log('Request body:', requestBody);
             const response = await axios.post('http://localhost:8080/auth/signup', requestBody);
-            console.log(response.data);
             alert('Signup successful');
+            if (role) {
+                navigate('/driver', { state: { driverId: id } });
+            } else {
+                navigate('/passenger', { state: { passengerId: id } });
+            }
         } catch (error) {
             console.error('Error signing up:', error);
             alert('Error signing up');
@@ -40,45 +57,45 @@ const SignupPage = () => {
     };
 
     return (
-        <form onSubmit={handleSubmit}>
+        <div>
+            <label>Name:</label>
+            <input type="text" name="name" value={name} onChange={(e) => setName(e.target.value)} />
+            <br />
+
+            <label>Email:</label>
+            <input type="email" name="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+            <br />
+
+            <label>Phone Number:</label>
+            <input type="tel" name="phone" value={phone} onChange={(e) => setPhone(e.target.value)} />
+            <br />
+
+            <label>Username:</label>
+            <input type="text" name="username" value={username} onChange={(e) => setUsername(e.target.value)} />
+            <br />
+
+            <label>Password:</label>
+            <input type="password" name="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+            <br />
+
+            <label>You really want to drive?</label>
             <input
-                type="text"
-                value={username}
-                onChange={(e) => setUsername(e.target.value)}
-                placeholder="Username"
+                type="checkbox"
+                checked={role}
+                onChange={(e) => setRole(e.target.checked)}
             />
-            <input
-                type="password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                placeholder="Password"
-            />
-            <input
-                type="text"
-                value={name}
-                onChange={(e) => setName(e.target.value)}
-                placeholder="Name"
-            />
-            <input
-                type="text"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                placeholder="Email"
-            />
-            <input
-                type="text"
-                value={phone}
-                onChange={(e) => setPhone(e.target.value)}
-                placeholder="Phone"
-            />
-            <input
-                type="text"
-                value={car}
-                onChange={(e) => setCar(e.target.value)}
-                placeholder="Car"
-            />
-            <button type="submit">Sign Up</button>
-        </form>
+            <br />
+
+            {role && (
+                <>
+                    <label>Car:</label>
+                    <input type="text" name="car" value={car} onChange={(e) => setCar(e.target.value)} />
+                    <br />
+                </>
+            )}
+
+            <button onClick={handleSubmit}>Sign Up</button>
+        </div>
     );
 };
 
--- slavic-ride-java/src/main/java/Slavic/Ride/MMM/Resource/AuthResource.java
+++ slavic-ride-java/src/main/java/Slavic/Ride/MMM/Resource/AuthResource.java
@@ -4,26 +4,33 @@ import Slavic.Ride.MMM.Service.DriverService;
 import Slavic.Ride.MMM.Service.PassengerService;
 import Slavic.Ride.MMM.User.Driver;
 import Slavic.Ride.MMM.User.Passenger;
+import Slavic.Ride.MMM.User.User;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import java.util.UUID;
 
 import java.util.Map;
 
+@Slf4j
 @RestController
 @RequestMapping("/auth")
+@RequiredArgsConstructor
 public class AuthResource {
-    private DriverService driverService;
-    private PassengerService passengerService;
+    private final DriverService driverService;
+    private final PassengerService passengerService;
 
     @GetMapping("/generateId")
     public ResponseEntity <?> generate () {
-        return ResponseEntity.ok("id has been generated");
+        System.out.println("id has been generated");
+        return ResponseEntity.ok(UUID.randomUUID().toString());
     }
 
     @PostMapping("/signup")
     public ResponseEntity <?> signup (@RequestBody Map<String, Object> user) {
-        System.out.println(user);
-        if (user.get("car") != null) {
+        System.out.println(user.get("car"));
+        if (user.get("car") != "") {
             Driver driver = driverService.createDriver(new Driver(
                     (String) user.get("name"),
                     (String) user.get("email"),
@@ -33,10 +40,17 @@ public class AuthResource {
             ));
             driver.setUsername((String) user.get("username"));
             driver.setPassword((String) user.get("password"));
-
             return ResponseEntity.ok(driverService.createDriver(driver));
         } else {
-            return ResponseEntity.ok(passengerService.createPassenger((Passenger) user));
+            Passenger passenger = passengerService.createPassenger(new Passenger(
+                    (String) user.get("name"),
+                    (String) user.get("email"),
+                    (String) user.get("phone"),
+                    (String) user.get("id")
+            ));
+            passenger.setUsername((String) user.get("username"));
+            passenger.setPassword((String) user.get("password"));
+            return ResponseEntity.ok(passengerService.createPassenger(passenger));
         }
     }
 }
